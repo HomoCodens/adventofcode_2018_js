09782b942a7a39c697fe4593d12caa3729558e8a
Severin Thöni <sethoeni@codeuntamed.ch>
2018-12-01 08:33:17 +0100

    Solve day 1

--- solvers/day1.js
+++ solvers/day1.js
@@ -1,3 +1,17 @@
 exports.solver = function(input) {
-  return null;
+
+  const changes = input.split('\n').
+                          map((x) => Number.parseInt(x));
+  const resultingFrequency = changes.reduce((s, e) => s + e);
+
+  let visitedFreqs = [];
+  let runningFreq = 0;
+  let i = 0;
+  while(visitedFreqs.indexOf(runningFreq += changes[i]) === -1) {
+    visitedFreqs.push(runningFreq);
+    i = (i + 1) % changes.length;
+  }
+
+  return `The final frequency is ${resultingFrequency}.
+When looping, the first double frequency is ${runningFreq}.`;
 }
