477e7c219c7b4952f624c82c69ede6d72b46dffe
Severin Thöni <sethoeni@codeuntamed.ch>
2018-12-12 07:59:41 +0100

    Begin day12

--- solvers/day12.js
+++ solvers/day12.js
@@ -1,3 +1,13 @@
 exports.solver = function(input) {
-  return null;
+  input = input.split('\n');
+  let pots = input[0].match(/[.#]+/)[0].
+                      split('').
+                      reduce((s, e, i) => {
+                        s[i] = e === '#';
+                        return s;
+                      }, {});
+  const rules = input.slice(2);
+
+  console.log(pots);
+  console.log(rules);
 }
